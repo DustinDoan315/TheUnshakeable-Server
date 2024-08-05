1086b1789cf8e71a2566814257f880367192971b
Dustin Doan <doankhanhdong3105@gmail.com>
2024-08-05 08:29:02 +0700

    Remove node_modules from git tracking

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const cors = require("cors");
+const PORT = 4000;
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+const generateID = () => Math.random().toString(36).substring(2, 10);
+
+socketIO.on("connection", (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+
+  socket.on("disconnect", () => {
+    socket.disconnect();
+    console.log("🔥: A user disconnected");
+  });
+});
+
+app.get("/api", (req, res) => {
+  res.json({
+    name: "Dustin",
+    title: "Title",
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
